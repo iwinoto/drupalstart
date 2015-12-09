7445c426361eec852589979f8dc6c1ea9e7fb82a
Iwan Winoto <iwinoto@gmail.com>
2015-12-10 00:49:47 +1100

    implode URIs array

--- bluezone/configtweaks/settings.php
+++ bluezone/configtweaks/settings.php
@@ -618,5 +618,5 @@ $databases = array (
 );
 
 $conf['site_name'] = 'Metlife Drupal site!!';
-$conf['site_slogan'] = 'Don\'t be a droop, Drupal it! (' . $application_json["application_uris"] . '; ' . $application_json["name"] . ')';
+$conf['site_slogan'] = 'Don\'t be a droop, Drupal it! (' . implode("; ", $application_json["application_uris"]) . '; ' . $application_json["name"] . ')';
 
