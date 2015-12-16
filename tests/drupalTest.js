6f68aeca801e2435a22d3ee5f5f3c5a8888123fe
Iwan Winoto <iwinoto@gmail.com>
2015-12-16 14:52:55 +1100

    Simple tests with nodeUnit

---
+++ tests/drupalTest.js
@@ -0,0 +1,51 @@
+/**
+ * Modules for tests
+ */
+/* jshint unused: false */
+var nodeunit = require('nodeunit'),
+    https = require('https'),
+    util = require('util'),
+    remote = process.env.TEST_REMOTE === 'true';
+
+console.log(process.env.VCAP_APPLICATION);
+
+//var base_uri = 'https://' + JSON.parse(process.env.VCAP_APPLICATION).uris[0];
+
+//var vcap_application = JSON.parse(process.env.VCAP_APPLICATION);
+
+exports.testcase = {
+  setUp: function (callback) {
+    //set up
+    console.log("startUp");
+    callback();
+  },
+  tearDown: function (callback) {
+    //cleanup
+    console.log("tearDown");
+    callback();
+  },
+  
+  testSomething: function (test) {
+    test.expect(1);
+    test.ok(true, "Test should pass");
+    test.done();
+  }
+
+  /*
+  testGetRequest: function (test) {
+    test.expect(1);
+    
+    base_uri = vcap_application.application_uris[0];
+    https.get("https://" + base_uri, function(res){
+    	test.ok(res.statusCode < 200 || response.statusCode > 299);
+    	test.done;
+    	response.on('data', function(chunk) {
+            console.log("GET got ", chunk.length, " characters.");
+    	});
+    }).on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+
+  }
+*/
+};
